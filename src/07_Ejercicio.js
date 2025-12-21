b4f9f703c920560d4fe84d6702a0407220b632a1
Milton Mamani <miltin.mamani010@gmail.com>
2025-12-21 20:53:49 -0300

    agrege el ejercicio numero 07,

---
+++ src/07_Ejercicio.js
@@ -0,0 +1,9 @@
+let num1, num2, num3,Mayor;
+
+num1 = parseInt(prompt(" Ingrese el primer número:"));
+num2 = parseInt(prompt(" Ingrese el segundo número:"));
+num3 = parseInt(prompt(" Ingrese el tercer número:"));
+
+Mayor = Math.max(num1, num2, num3);
+
+document.writeln(" El número mas grande es: " + Mayor); 
