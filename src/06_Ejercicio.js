4d4d2538d84e99124c66591618c7a9b8aa53488c
Milton Mamani <miltin.mamani010@gmail.com>
2025-12-21 20:48:16 -0300

    agrege el ejercicion numero 06, este consiste en que el usuario ingrese dos numeros y el numero mayor , o mas grande sea mostrado por pantalla

---
+++ src/06_Ejercicio.js
@@ -0,0 +1,10 @@
+let num1, num2;
+num1 = parseInt(prompt('Ingrese el primer número:'));
+num2 = parseInt(prompt('Ingrese el segundo número:'));
+
+if ( num1 > num2 ){
+    document.writeln(' El número mayor es: ' + num1);
+} else {
+    document.writeln(' El número mayor es: ' + num2);
+
+}
